325774be204e29ae156bbc32b9b4b27c704da887
Maksym Kupchenko <maxkup19@gmail.com>
2023-03-02 13:51:04 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Maksym Kupchenko on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
